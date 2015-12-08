8d2a3b7972691360028d86c953b83bf01531b98a
Terrell Russell <terrellrussell@gmail.com>
2015-12-08 11:54:33 -0500

    comment out webdav for check

--- ec2-irods-demo/per-boot/update-fqdn-everywhere.sh
+++ ec2-irods-demo/per-boot/update-fqdn-everywhere.sh
@@ -6,18 +6,17 @@ if [ -e $FQDN_LOCATION ] ; then
 fi
 NEW_FQDN=`ec2metadata --public-hostname`
 
-# if the hostname has changed
-if [ "$NEW_FQDN" != "$OLD_FQDN" ] ; then
-    # write it down
-    echo $NEW_FQDN > $FQDN_LOCATION
+# update hostname
+sudo hostname $NEW_FQDN
+sudo su -c "echo $NEW_FQDN > /etc/hostname"
 
-    # update hostname
-    sudo hostname $NEW_FQDN
-    sudo su -c "echo $NEW_FQDN > /etc/hostname"
-
+# don't run on first boot
+if [ -e $FQDN_LOCATION ] ; then
     # update the irods service account's user environment
-    sudo -u irods bash -c 'sed  -i "s|\(.*irods_host.*\)'$OLD_FQDN'\(.*\)|\1'$NEW_FQDN'\2|g" ~irods/.irods/irods_environment.json'
-
+    sudo -u irods bash -c 'sed -i "s|\(.*irods_host.*\)'$OLD_FQDN'\(.*\)|\1'$NEW_FQDN'\2|g" ~irods/.irods/irods_environment.json'
     # update the resource host information
     sudo su - irods -c "iadmin modresc demoResc host $NEW_FQDN"
 fi
+
+# write it down
+echo $NEW_FQDN > $FQDN_LOCATION
